53cab0cc40b32cc0d28e42f1acfd620d11adf8a3
zildjansssssssss <gargar.zildjan03272003@gmail.com>
2024-10-09 22:13:32 -0700

    Add files via upload

---
+++ performance.php
@@ -0,0 +1,26 @@
+<?php include 'includes/header.php'; ?>
+<?php include 'includes/navbar.php'; ?>
+<?php include 'includes/sidebar.php'; ?>
+                            <div class="sb-sidenav-menu-heading">Future Features</div>
+                            <a class="nav-link" href="charts.html">
+                                <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
+                                Charts
+                            </a>
+                            <a class="nav-link" href="tables.html">
+                                <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                                Tables
+                            </a>
+                        </div>
+                    </div>
+                    
+                    <div id="layoutSidenav_content">
+                <main>
+                    
+                        
+                            
+                         
+                        
+                </main>
+                
+                <?php include 'includes/footer.php'; ?>
+                <?php include 'includes/script.php'; ?>
